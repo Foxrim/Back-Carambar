f63e07e994d5a7913dbe94e097a51349e9094d9b
Foxrim <Flavien085@gmail.com>
2025-05-18 19:24:10 +0200

    [fix] - github page add cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -3,9 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const allowedOrigins = [`http://${process.env.HOST}:${process.env.PORT}`, `${process.env.BACK_LINK}`];
+const allowedOrigins = [
+  `http://${process.env.HOST}:${process.env.PORT}`,
+  process.env.BACK_LINK || '',
+  process.env.GITHUB_PAGE || '',
+  process.env.HOST_DEV || '',
+].filter(Boolean);
 
-export const corsOptions: cors.CorsOptions = {
+const corsOptions: cors.CorsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
