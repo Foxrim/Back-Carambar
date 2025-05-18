837ddea5c2084b928d009c74fcee0a07ffe2923d
Foxrim <Flavien085@gmail.com>
2025-05-18 19:47:52 +0200

    test ça devient long là

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -6,12 +6,13 @@ dotenv.config();
 const allowedOrigins = [
   `http://${process.env.HOST}:${process.env.PORT}`,
   process.env.BACK_LINK || '',
-  process.env.GITHUB_PAGE || '',
+  process.env.GITHUB_PAGE || 'https://foxrim.github.io',
   process.env.HOST_DEV || '',
 ].filter(Boolean);
 
 const corsOptions: cors.CorsOptions = {
   origin: (origin, callback) => {
+    console.log(allowedOrigins)
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
